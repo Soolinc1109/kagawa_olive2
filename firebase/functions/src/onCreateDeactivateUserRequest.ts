3a94a1f09e1c18d9cbd1f2d5b985f02ef80da382
“akiraueda” <sool.hikaru@gmail.com>
2022-09-09 23:04:00 +0900

    second

--- firebase/functions/src/onCreateDeactivateUserRequest.ts
+++ firebase/functions/src/onCreateDeactivateUserRequest.ts
@@ -5,12 +5,13 @@ export const onCreateDeactivateUserRequest = functions
     .region(`asia-northeast1`)
     .firestore.document(`/deactivateUserRequests/{deactivateUserRequestId}`)
     .onCreate(async (snapshot, context) => {
-        functions.logger.info(`あいう`)
         const data = snapshot.data()
-        const documentId = context.params.deactivateUserRequestId
-        const count = data.count
-        functions.logger.info(`${count}`)
-        await admin.firestore().collection('deactivateUserRequests').doc(documentId).update({
-            'count': count + 4
-        })
+        const uid = data.uid
+        try {
+            await admin.auth().updateUser(uid,{
+                disabled: true
+            })
+        } catch (e) {
+            functions.logger.info(`アカウント停止失敗 ${uid}`)
+        }
     })
